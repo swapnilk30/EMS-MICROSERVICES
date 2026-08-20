f9f623e1dcab3a33da2417d534d3d4c42aa651bf
swapnilk30 <sk9137587676@gmail.com>
2026-08-20 14:10:45 +0530

    config client

--- backend/employee-service/src/main/java/com/example/employee/service/controller/EmployeeController.java
+++ backend/employee-service/src/main/java/com/example/employee/service/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.employee.service.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -31,6 +32,18 @@ public class EmployeeController {
 
 	private final EmployeeService employeeService;
 	private final AddressServiceClient addressServiceClient;
+	
+	
+	
+    @Value("${application.message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
+    }
+    
+    
 	
 	@PostMapping
 	public ResponseEntity<?> createEmployee(@Valid @RequestBody EmployeeDto employeeDto){
